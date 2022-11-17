dee106fcb44c0ff50d48d3bfc3786fcfe2cc95e3
Adedayo Agboola <kreativedayo@gmail.com>
2022-11-17 02:19:51 +0100

    gaussian & binomial distributions
    Updated packages

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="distributions",
+    version="0.1",
+    description="Gaussian & Binomial Distributions",
+    packages=["distributions"],
+    zip_safe=False,
+)
